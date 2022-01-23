98930220fa1f18d3c541847149be2d47e30b4174
damien@ordi-fred <garreau.damien.jy@gmail.com>
2022-01-23 12:57:13 +0100

    evolution scripts

---
+++ compute_evolution.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+
+Computing the raw results for Figure 6, 14, 15, and 16. We look at the number 
+of superpixels and local maxima in a rectangle at distance 2*kernel_width from 
+the border to totally ignore the border effects. Results are saved in the 
+folder result_folder
+
+"""
+
+import numpy as np
+import sys
+import os
+import pickle
+import time
+
+from skimage.color import rgb2lab
+from skimage.segmentation import quickshift
+
+from utils.aux_functions import get_local_max
+from utils.aux_functions import convert_to_2D
+from utils.aux_functions import count_local_max
+from utils.aux_functions import get_result_path
+from utils.aux_functions import mkdir
+from utils.aux_functions import image_shape_helper
+
+from utils.theory import compute_approx_expec
+
+DBL_MAX = sys.float_info.max
+
+# default values: kernel_size = 5, kernel_width = 15, max_dist = 10
+kernel_size = 10
+kernel_width = 3*kernel_size
+max_dist = 20
+
+# shape of the image and noise level
+sigma = 5.0
+ratio = 1.0
+
+# number of repetitions to get error bars
+# NOTE: already a lot of randomness in the process, not a lot of reps are needed
+n_rep = 10
+
+# background color
+# NOTE: does not change anything except if we want to plot the original image
+background_color = (100,100,100)
+
+# size of the images
+# NOTE: - 28 is the MNIST standard
+# - 299 is the imagenet standard
+# - should start at 4*kernel_width minimum to avoid any problem
+min_shape = 150
+max_shape = 300
+n_exp = max_shape - min_shape
+n_seg_store = np.zeros((n_exp,n_rep),dtype=int)
+n_max_store = np.zeros((n_exp,n_rep),dtype=int)
+
+t_start = time.time()
+
+for i_exp in range(n_exp):
+    
+    # current input shape
+    input_shape = min_shape + i_exp
+    height,width = image_shape_helper(input_shape,ratio=ratio)
+    print("size {},{}, kernel_size = {}, max_dist = {}, sigma = {}".format(height,width,kernel_size,max_dist,sigma))
+    
+    h_min = int(2*kernel_width)
+    h_max = int(height - 2*kernel_width)
+    w_min = int(2*kernel_width)
+    w_max = int(width - 2*kernel_width)
+    
+    for i_rep in range(n_rep):
+        
+        # create the image
+        xi_array = np.ones((height,width,3),dtype=float) * background_color
+    
+        # converting to lab space
+        xi_rgb   = np.uint8(xi_array)
+        xi_lab = rgb2lab(xi_rgb)
+
+        # adding some noise to the pixel values in the lab space
+        xi_lab += np.random.normal(0,sigma,(height,width,3))
+        
+        # computing the segmentation
+        out = quickshift(xi_lab,
+                                 max_dist=max_dist,
+                                 kernel_size=kernel_size,
+                                 random_seed=i_rep,
+                                 convert2lab=False,
+                                 return_tree=True)
+        my_seg  = out[0]
+        my_tree = np.asarray(out[1])
+        
+        n_seg = len(np.unique(my_seg[h_min:h_max,w_min:w_max]))
+        print("     -> {} superpixels".format(n_seg))
+        n_seg_store[i_exp,i_rep] = n_seg
+        
+        local_max = get_local_max(my_seg,my_tree)
+        local_max_2D = convert_to_2D(local_max,height,width)
+        n_max = count_local_max(local_max_2D,h_min,h_max,w_min,w_max)
+        print("     -> {} local max".format(n_max))
+        n_max_store[i_exp,i_rep] = n_max
+    
+# ncomputing the theory
+approx_store = np.zeros((n_exp,))
+for i_exp in range(n_exp):
+
+    input_shape = min_shape + i_exp
+    height,width = image_shape_helper(input_shape,ratio=ratio)
+    h_min = 2*kernel_width
+    h_max = height - 2*kernel_width
+    w_min = 2*kernel_width
+    w_max = width - 2*kernel_width
+    approx_theo = compute_approx_expec(height,
+                                     width,
+                                     h_min,h_max,w_min,w_max,
+                                     kernel_size=kernel_size,max_dist=max_dist)
+    approx_store[i_exp] = approx_theo
+
+
+t_end = time.time()
+print("elapsed = {}".format(t_end-t_start))
+print()
+
+# saving the results
+result_folder = "../results/evolution/"
+result_path =  get_result_path(result_folder,kernel_size,max_dist)
+mkdir(result_path)
+aux_str = "sigma_" + str(sigma) + "_ratio_" + str(ratio) + "_min_" + str(min_shape) + "_max_" + str(max_shape)
+pickle_name = os.path.join(result_path,aux_str + ".pkl")
+print("saving results...")
+with open(pickle_name,'wb') as f:
+    pickle.dump([kernel_size,max_dist,sigma,ratio,min_shape,max_shape,n_seg_store,n_max_store,approx_store],f)
+print("done!")
+print()
+
---
+++ plot_evolution.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+This script produces the plots from Figure 6, 14, 15, and 16 of the paper. 
+
+IMPORTANT: first compute the raw results by running 
+
+"""
+
+import sys
+import pickle
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utils.aux_functions import format_helper
+from utils.aux_functions import get_result_path
+
+DBL_MAX = sys.float_info.max
+
+# params
+kernel_size = 5
+ratio = 1.0
+min_shape = 150
+max_shape = 300
+n_exp = max_shape-min_shape
+shape_store = np.arange(min_shape,max_shape)
+sigmas = np.array([0.001,1])
+n_sigmas = sigmas.shape[0]
+dms = np.array([DBL_MAX,18,10])
+n_dm = dms.shape[0]
+n_rep = 10
+
+n_seg_store    = np.zeros((n_exp,n_rep,n_dm,n_sigmas))
+n_max_store    = np.zeros((n_exp,n_rep,n_dm))
+n_approx_store = np.zeros((n_exp,n_dm))
+
+# where to look for the results
+result_folder = "../results/evolution/"
+
+# loading results
+for i_dist in range(n_dm):
+    max_dist = dms[i_dist]
+    
+    result_path =  get_result_path(result_folder,kernel_size,max_dist)
+    
+    for i_sig in range(n_sigmas):
+        
+        sigma = sigmas[i_sig]
+        aux_str = "sigma_" + str(sigma) + "_ratio_" + str(ratio) + "_min_" + str(min_shape) + "_max_" + str(max_shape)
+        pickle_name = os.path.join(result_path,aux_str + ".pkl")
+        with open(pickle_name,'rb') as f:
+            ks,md,sig,rat,min_s,max_s,aux_n_seg,aux_n_max,aux_approx = pickle.load(f)
+
+
+        # checking that everything corresponds
+        if kernel_size != ks or sig != sigma or rat != ratio:
+            print("something is wrong, input parameters do not correspond to what is stored")
+            sys.exit()
+
+        # store everything
+        n_seg_store[:,:,i_dist,i_sig] = aux_n_seg
+        if i_sig == 0:
+            n_max_store[:,:,i_dist] = aux_n_max
+            n_approx_store[:,i_dist] = aux_approx
+
+##############################################################################
+
+# force vector fonts
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+big_lw = 3
+small_lw = 1.5
+my_alpha = 0.2
+big_fs = 25
+small_fs = 15
+
+fig,ax = plt.subplots(1,n_dm,figsize=(10*n_dm,7))    
+
+for i_dist in range(n_dm):
+
+    # plotting the number of local maxima
+    mean_local_max = np.mean(n_max_store[:,:,i_dist],1)
+    loc_line, = ax[i_dist].plot(shape_store,mean_local_max,color='b')
+    std_local_max = np.std(n_max_store[:,:,i_dist],1)
+    ax[i_dist].fill_between(shape_store,mean_local_max-std_local_max,mean_local_max+std_local_max,facecolor='b',alpha=my_alpha)
+    loc_line.set_label(r"average number of local maxima ($\sigma= {}$)".format(sigmas[0]))
+    
+    # plotting the number of superpixels, small sigma
+    mean_n_seg = np.mean(n_seg_store[:,:,i_dist,0],1)
+    seg_line_small, = ax[i_dist].plot(shape_store,mean_n_seg,color='k',linestyle='-')
+    std_n_seg= np.std(n_seg_store[:,:,i_dist,0],1)
+    ax[i_dist].fill_between(shape_store,mean_n_seg-std_n_seg,mean_n_seg+std_n_seg,facecolor='k',alpha=my_alpha)
+    seg_line_small.set_label(r"average number of superpixels ($\sigma={}$)".format(sigmas[0]))
+    
+    # plotting the number of superpixels, large sigma
+    mean_n_seg = np.mean(n_seg_store[:,:,i_dist,1],1)
+    seg_line_large, = ax[i_dist].plot(shape_store,mean_n_seg,color='k',linestyle='--')
+    std_n_seg= np.std(n_seg_store[:,:,i_dist,1],1)
+    ax[i_dist].fill_between(shape_store,mean_n_seg-std_n_seg,mean_n_seg+std_n_seg,facecolor='k',alpha=my_alpha)
+    seg_line_large.set_label(r"average number of superpixels ($\sigma={}$)".format(sigmas[1]))
+ 
+    
+    # plotting the theory
+    th_line, = ax[i_dist].plot(range(min_shape,max_shape),n_approx_store[:,i_dist],color='r',linewidth=big_lw)
+    th_line.set_label("approximation")
+    
+    # bigger fonts
+    ax[i_dist].tick_params(axis='x',labelsize=small_fs)
+    ax[i_dist].tick_params(axis='y',labelsize=small_fs)
+
+    # legend
+    ax[i_dist].legend(fontsize=small_fs,loc=2)
+
+    # title
+    s_kernel_size,s_max_dist = format_helper(kernel_size,dms[i_dist])
+    s_title = r"kernel_size $=$ " + s_kernel_size + ", max_dist $=$ " + s_max_dist 
+    ax[i_dist].set_title(s_title,fontsize=big_fs)
+
+    # xlabel
+    ax[i_dist].set_xlabel("rectangle width",fontsize=small_fs)
+    
+    # dropping common scale
+    #ax[i_dist].set_ylim(0,np.max())
+
+# saving the figure
+#fig_name = "../figures/final_evolution_ks_" + s_kernel_size + ".pdf"
+#fig.savefig(fig_name,format='pdf',bbox_inches = 'tight',pad_inches = 0)
+
+    
